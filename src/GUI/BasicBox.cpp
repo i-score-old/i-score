6ce429425fbd279a8e28032da807bee176a96c27
nicolashincker <nicolash24@hotmail.com>
2012-10-25 10:30:32 +0200

    test branch

--- src/GUI/BasicBox.cpp
+++ src/GUI/BasicBox.cpp
@@ -79,6 +79,7 @@ using std::string;
 using std::vector;
 using std::map;
 
+
 const int BasicBox::COMBOBOX_HEIGHT = 25;
 const int BasicBox::COMBOBOX_WIDTH = 120;
 const float BasicBox::TRIGGER_ZONE_WIDTH = 15.;
