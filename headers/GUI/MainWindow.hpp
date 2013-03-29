7d3def3b2c9878179dc17df8d4feedc92f6db42f
Jaime Chao <jaimitochao@gmail.com>
2013-03-29 07:50:13 +0100

    changes in i-score.pro and others

--- headers/GUI/MainWindow.hpp
+++ headers/GUI/MainWindow.hpp
@@ -91,6 +91,7 @@ class MainWindow : public QMainWindow
    * \param warningLevel : the level of the message
    */
   void displayMessage(const QString &message, unsigned int warningLevel);
+    
   /*!
    * \brief Selects interaction mode
    *
@@ -98,26 +99,31 @@ class MainWindow : public QMainWindow
    * \param boxMode : the new box creation mode
    */
   void selectMode(const InteractionMode &mode,const BoxCreationMode &boxMode = NB_MODE);
+    
   /*!
    * \brief Shows/Hides the editor.
    *
    * \param state : the editor active state
    */
   void showEditor(bool state);
+    
   /*!
    * \brief Determines if a modification occured since the last save.
    *
    * \return true if a modification occured since document was saved.
    */
   bool documentModified() const;
+    
   /*!
    * \brief Called when the end of the composition is reached.
    */
   void timeEndReached();
+    
   /*!
    * \brief Returns goto value in ms.
    */
   int gotoValue();
+    
   void setMaquetteSceneTitle(QString name);
   inline MaquetteWidget *maquetteWidget(){return _maquetteWidget;}
 
@@ -137,88 +143,111 @@ class MainWindow : public QMainWindow
    * \brief Opens a network configuration window.
    */
   void networkConfig();
+    
   /*!
    * \brief Resets current composition and prepares to write into a new file.
    */
   void newFile();
+    
   /*!
    * \brief Opens an existing file.
    */
   void open();
+    
   /*!
    * \brief Saves current file.
    */
   bool save();
+    
   /*!
    * \brief Saves the current file into a new one.
    */
   bool saveAs();
+    
   /*!
    * \brief Creates a picture from the current composition.
    */
   void exporting();
+    
   /*!
    * \brief Prints the current composition.
    */
   void print();
+    
   /*!
    * \brief Opens "About" dialog box.
    */
   void about();
+    
   /*!
    * \brief Opens "Help" dialog box.
    */
   void help();
+    
   /*!
    * \brief Updates the palette view state.
    */
   void updateEditor();
+    
   /*!
    * \brief Updates the tracks view.
    */
   void viewTrack();
+    
   /*!
    * \brief Cut the current selection of boxes.
    */
   void cutSelection();
+    
   /*!
    * \brief Copies the current selection of boxes.
    */
   void copySelection();
+    
   /*!
    * \brief Pastes the current selection of boxes.
    */
   void pasteSelection();
+    
   /*!
    * \brief Starts playing the composition.
    */
   void play();
+    
   /*!
    * \brief Pauses playing the composition.
    */
   void pause();
+    
   /*!
    * \brief Stops playing the composition.
    */
   void stop();
+    
   /*!
    * \brief Changes the current playing mode.
    */
   void playModeChanged();
+    
   /*!
    * \brief Raised when the acceleration slider value is changed.
    */
   void accelerationChanged(int value);
+    
   void accelerationValueEntered(double value);
+    
   /*!
    * \brief Raised when the goto slider value is changed.
    */
   void gotoChanged();
+    
   void gotoValueEntered(double value);
+    
   /*!
    * \brief Switched current interaction mode.
    */
   void selectMode();
+    
   /*!
    * \brief Selects the whole set of boxes.
    */
@@ -229,32 +258,39 @@ class MainWindow : public QMainWindow
    * \brief Initializes actions.
    */
   void createActions();
+    
   /*!
    * \brief Initializes menus.
    */
   void createMenus();
+    
   /*!
    * \brief Initializes toolbars.
    */
   void createToolBars();
+    
   /*!
    * \brief Creates a status bar.
    */
   void createStatusBar();
+    
   /*!
    * \brief Reads persistent settings for our application, see QSettings doc.
    */
   void readSettings();
+    
   /*!
    * \brief Writes persistent settings for our application, see QSettings doc.
    */
   void writeSettings();
+    
   /*!
    * \brief Loads a file.
    *
    * \param fileName : the file to open
    */
   void loadFile(const QString &fileName);
+    
   /*!
    * \brief Saves a file.
    *
@@ -263,12 +299,14 @@ class MainWindow : public QMainWindow
    * \return true if the file was saved successfully
    */
   bool saveFile(const QString &fileName);
+    
   /*!
    * \brief Updates current file name.
    *
    * \param fileName the new file name
    */
   void setCurrentFile(const QString &fileName);
+    
   /*!
    * \brief Gets stripped name.
    *
