fc4d77e12ff23725b56cf9516b7ed371288416c2
Jaime Chao <jaimitochao@gmail.com>
2013-04-12 19:00:08 +0200

    Doxygen index corrected

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,25 @@
  *
  * \section intro_sec Introduction
  *
- * i-score is an intermedia sequencer for the precise and flexible scripting of interactive scenarios. 
+ * i-score is an intermedia sequencer for the precise and flexible scripting of interactive scenarios.
  * In contrast to most sequencers, i-score doesn’t produce any media, but controls other environments’ parameters, by creating  snapshots and automations, and organizing them in time in a multi-linear way.
  *
-\section doc_sec Documentation
-Using i-score requires a third-party environment for media generation.
-
-At the current time, it can be used with pure-data or Cycling’74 Max. \n
-
-In order to control them with i-score, you need to expose your patcher’s parameters with the Minuit protocol. This can be easily done by inserting any of the interfaces available below in your patcher, and organizing your parameters as a tree-structure.
-
-As of now, interfaces with Minuit are available for the following environments :
-\li \c AlignLeft Jamoma 0.6 for Max
-\li \c AlignLeft Jamoma 0.5 for Max
-\li \c AlignLeft tapemovie in Max (embedded)
-\li \c AlignLeft pattrstorage in Max
-\li \c AlignLeft pure-data
-
-More info available at : http://i-score.org/
-
+ * \section doc_sec Documentation
+ * Using i-score requires a third-party environment for media generation.
+ *
+ * At the current time, it can be used with pure-data or Cycling’74 Max. \n
+ *
+ * In order to control them with i-score, you need to expose your patcher’s parameters with the Minuit protocol. This can be easily done by inserting any of the interfaces available below in your patcher, and organizing your parameters as a tree-structure.
+ *
+ * As of now, interfaces with Minuit are available for the following environments :
+ * \li \c Jamoma 0.6 for Max
+ * \li \c Jamoma 0.5 for Max
+ * \li \c tapemovie in Max (embedded)
+ * \li \c pattrstorage in Max
+ * \li \c pure-data
+ *
+ * More info available at : http://i-score.org/
+ *
  * \section install_sec Installation
  *
  * Download the source at : https://github.com/nicolashincker/i-score
