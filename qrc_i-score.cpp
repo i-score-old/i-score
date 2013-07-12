318e1c672f684eb6066f6b2a55d550f01cf64a43
theod <theod@gmea.net>
2013-07-12 16:37:44 +0200

    updating qrc file

--- qrc_i-score.cpp
+++ qrc_i-score.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Resource object code
 **
-** Created: Thu Jul 11 17:34:10 2013
+** Created: Fri Jul 12 15:54:28 2013
 **      by: The Resource Compiler for Qt version 4.8.3
 **
 ** WARNING! All changes made in this file will be lost!
