d1eb7bc962ed95552cce8b87408e80b45ac009d0
Nicolas HINCKER <nicolash24@hotmail.com>
2013-11-08 09:25:08 +0100

    updating recorded curves during execution

--- src/GUI/MaquetteScene.cpp
+++ src/GUI/MaquetteScene.cpp
@@ -1559,6 +1559,12 @@ MaquetteScene::updatePlayingBoxes()
 
   for (it = _playingBoxes.begin(); it != _playingBoxes.end(); ++it) {
       it->second->update();
+
+      //updating recorded curves
+      QList<std::string> recMsgs = static_cast<AbstractBox *>(it->second->abstract())->messagesToRecord();
+      if(!recMsgs.isEmpty())
+          for(int i=0 ; i<recMsgs.size() ; i++)
+              it->second->updateCurve(recMsgs.at(i),true);
     }
 }
 
