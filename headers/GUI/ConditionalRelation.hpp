13803d5283bcf763dafee41bcbdd984e528bbd73
Nicolas HINCKER <nicolash24@hotmail.com>
2014-03-24 16:03:42 +0100

    adding basic methods headers

--- headers/GUI/ConditionalRelation.hpp
+++ headers/GUI/ConditionalRelation.hpp
@@ -1,4 +1,60 @@
 #ifndef CONDITIONALRELATION_HPP
 #define CONDITIONALRELATION_HPP
 
+/*!
+ * \file ConditionalRelation.hpp
+ *
+ * \author Nicolas Hincker
+ */
+
+#include <QGraphicsItem>
+
+class MaquetteScene;
+class BasicBox;
+
+class ConditionalRelation : public QGraphicsItem
+{
+
+public :
+    ConditionalRelation(QList<BasicBox *> boxesAttached, MaquetteScene *parent);
+
+    /*!
+     * \brief Update relation extremities' coordinates.
+     */
+    void updateCoordinates();
+
+    /*!
+     * \brief Attach a box or a group of boxes to the conditional relation.
+     *
+     * \param conditionedBoxes : the boxes to attach.
+     */
+    void attachBox(QList<BasicBox *> conditionedBox);
+
+    /*!
+     * \brief detach a box or a group of boxes of the conditional relation.
+     *
+     * \param conditionedBoxes : the boxes to detach.
+     */
+    void detachBox(QList<BasicBox *> conditionedBox);
+    /*!
+     * \brief Painting method, redefinition of QGraphicsItem::paint().
+     *
+     * \param painter : the painter  used to draw
+     * \param options : array of options
+     * \param widget : the widget that is being painting on
+     */
+    virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
+                       QWidget *widget = 0);
+
+    static const QColor CONDITIONAL_RELATION_COLOR;
+
+private :
+
+    QList<BasicBox *> _boxesAttached;   //!< Conditioned boxes attached to the relation.
+    MaquetteScene * _scene;             //!< The scene containing relation.
+    QPointF _start;                     //!< The starting point of the relation.
+    QPointF _end;                       //!< The ending point of the relation.
+    QColor _color;                      //!< Color of the relation.
+};
+
 #endif // CONDITIONALRELATION_HPP
--- src/GUI/ConditionalRelation.cpp
+++ src/GUI/ConditionalRelation.cpp
@@ -1,1 +1,74 @@
+/*
+ * Copyright: LaBRI / SCRIME / L'Arboretum
+ *
+ * Authors: Pascal Baltazar, Nicolas Hincker, Luc Vercellin and Myriam Desainte-Catherine (as of 16/03/2014)
+ *
+ * iscore.contact@gmail.com
+ *
+ * This software is an interactive intermedia sequencer.
+ * It allows the precise and flexible scripting of interactive scenarios.
+ * In contrast to most sequencers, i-score doesn’t produce any media,
+ * but controls other environments’ parameters, by creating snapshots
+ * and automations, and organizing them in time in a multi-linear way.
+ * More about i-score on http://www.i-score.org
+ *
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+
+#include "ConditionalRelation.hpp"
+
+const QColor ConditionalRelation::CONDITIONAL_RELATION_COLOR = QColor(0, 0, 0);
+
+ConditionalRelation::ConditionalRelation(QList<BasicBox *> boxesAttached, MaquetteScene *parent)
+    : QGraphicsItem(), _scene(parent), _boxesAttached(boxesAttached)
+{
+    _color = CONDITIONAL_RELATION_COLOR;
+}
+
+void
+ConditionalRelation::updateCoordinates()
+{
+
+}
+
+void
+ConditionalRelation::attachBox(QList<BasicBox *> conditionedBox){
+
+}
+
+void
+ConditionalRelation::detachBox(QList<BasicBox *> conditionedBox){
+
+}
+
+void
+ConditionalRelation::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+
+}
 
