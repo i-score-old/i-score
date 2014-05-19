e0073f4897df258973bd50bf94aa0e48464647eb
Nicolas HINCKER <nicolash24@hotmail.com>
2014-05-19 16:53:35 +0200

    adding +addANode at OSC device creation

--- src/GUI/DeviceEdit.cpp
+++ src/GUI/DeviceEdit.cpp
@@ -291,7 +291,7 @@ DeviceEdit::updateNetworkConfiguration()
         }
         if (_protocolChanged) {
             Maquette::getInstance()->setDeviceProtocol(_currentDevice.toStdString(), _protocolsComboBox->currentText().toStdString());
-            //          emit(deviceProtocolChanged(_protocolsComboBox->currentText()));
+//            emit(deviceProtocolChanged(_protocolsComboBox->currentText()));
         }
         emit(deviceChanged(_currentDevice));
     }
