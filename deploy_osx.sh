6a437b19ba37639e1752bbd210267e288420c99f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-01-05 14:23:20 -0800

    Take into account Jamoma dylib versionning; update gecode

--- deploy_osx.sh
+++ deploy_osx.sh
@@ -14,7 +14,7 @@ declare -a jamomalibs=("Foundation" "Modular" "DSP" "Score")
 declare -a jamomaexts=("Scenario" "Automation" "Interval" "Loop" "OSC" "Minuit" "MIDI" "AnalysisLib" "DataspaceLib" "FunctionLib" "System" "NetworkLib")
 for JAMOMA_LIB in "${jamomalibs[@]}"
 do
-	cp -rf /usr/local/jamoma/lib/libJamoma$JAMOMA_LIB.dylib $ISCORE_BUNDLE/Contents/Frameworks/jamoma/lib/
+	cp -rf /usr/local/jamoma/lib/libJamoma$JAMOMA_LIB.6.dylib $ISCORE_BUNDLE/Contents/Frameworks/jamoma/lib/
 done
 for JAMOMA_EXT in "${jamomaexts[@]}"
 do
@@ -34,5 +34,5 @@ GECODE_DIR=$(dirname $(find /usr -name libgecodekernel.dylib ! -path "*Cellar*"|
 declare -a gecodelibs=("kernel" "support" "int" "set" "driver" "flatzinc" "minimodel" "search" "float")
 for GECODE_LIB in "${gecodelibs[@]}"
 do
-	install_name_tool -change $GECODE_DIR/libgecode$GECODE_LIB.36.dylib @executable_path/../Frameworks/libgecode$GECODE_LIB.36.dylib $ISCORE_BUNDLE/Contents/Frameworks/jamoma/extensions/Scenario.ttdylib
+	install_name_tool -change $GECODE_DIR/libgecode$GECODE_LIB.39.dylib @executable_path/../Frameworks/libgecode$GECODE_LIB.39.dylib $ISCORE_BUNDLE/Contents/Frameworks/jamoma/extensions/Scenario.ttdylib
 done
